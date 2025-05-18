34154959c00f9bfc067289a866b1aa152dd1638d
warmaster.info <212267410+warmaster-info@users.noreply.github.com>
2025-05-18 21:49:31 +0200

    Github build action.

--- local-build.sh
+++ local-build.sh
@@ -8,6 +8,6 @@ set -e
 for lang in en; do
   echo "======= Generating $lang ======="
   rm -Rf output/$lang
-  docker run -u $(id -u) -e CI=true -e env=local -e date="$(date '+%Y-%m-%d %H:%M:%S %Z')" -v $PWD:/antora:Z --rm -t antora/antora:3.1.10 warmaster-playbook-en.yml --attribute env=local
+  docker run -u $(id -u) -e CI=true -e env=local -e date="$(date '+%Y-%m-%d %H:%M:%S %Z')" -v $PWD:/antora:Z --rm -t antora/antora:3.1.10 antora-playbook.yml --attribute env=local
 done
 echo
