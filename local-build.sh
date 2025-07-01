0a1c432046c4994ca44c300ce000b66af500f9b5
warmaster.info <212267410+warmaster-info@users.noreply.github.com>
2025-07-01 23:46:56 +0200

    "Better" (hackier) way to remove 03-fragment-jumper.js functionality.

--- local-build.sh
+++ local-build.sh
@@ -4,10 +4,22 @@ set -e
 
 [[ -d node_modules ]] || docker run --rm -t -u $(id -u) --volume $PWD:/antora:Z --env HOME=/antora antora/antora:3.1.10 npm i @antora/lunr-extension
 
-# Only generating English
 for lang in en; do
   echo "======= Generating $lang ======="
   rm -Rf output/$lang
   docker run -u $(id -u) -e CI=true -e env=local -e date="$(date '+%Y-%m-%d %H:%M:%S %Z')" -v $PWD:/antora:Z --rm -t antora/antora:3.1.10 antora-playbook.yml --attribute env=local
 done
 echo
+
+for lang in fr; do
+  echo "======= Generating $lang ======="
+  rm -Rf output/$lang
+  docker run -u $(id -u) -e CI=true -e env=local -e date="$(date '+%Y-%m-%d %H:%M:%S %Z')" -v $PWD:/antora:Z --rm -t antora/antora:3.1.10 antora-playbook_$lang.yml --attribute env=local
+done
+echo
+
+# Disable the functions in 03-fragment-jumper.js by breaking their event listeners.
+# At some point we will need to make a UI fork without this function,
+# or maybe it will be removed: https://gitlab.com/antora/antora-ui-default/-/issues/194
+sed -i 's/a\[href^="#"\]/DISABLEDXXXX/' output/*/_/js/site.js
+sed -i 's/window.addEventListener."load",function l.e./window.addEventListener("DSBL",function l(e)/' output/*/_/js/site.js
