1e726799b0e2a2ff9e94e10856229406d0f0f1f2
warmaster.info <212267410+warmaster-info@users.noreply.github.com>
2025-07-04 20:06:41 +0200

    Add experimental lists.

--- local-build.sh
+++ local-build.sh
@@ -4,14 +4,7 @@ set -e
 
 [[ -d node_modules ]] || docker run --rm -t -u $(id -u) --volume $PWD:/antora:Z --env HOME=/antora antora/antora:3.1.10 npm i @antora/lunr-extension
 
-for lang in en; do
-  echo "======= Generating $lang ======="
-  rm -Rf output/$lang
-  docker run -u $(id -u) -e CI=true -e env=local -e date="$(date '+%Y-%m-%d %H:%M:%S %Z')" -v $PWD:/antora:Z --rm -t antora/antora:3.1.10 antora-playbook.yml --attribute env=local
-done
-echo
-
-for lang in fr; do
+for lang in en fr; do
   echo "======= Generating $lang ======="
   rm -Rf output/$lang
   docker run -u $(id -u) -e CI=true -e env=local -e date="$(date '+%Y-%m-%d %H:%M:%S %Z')" -v $PWD:/antora:Z --rm -t antora/antora:3.1.10 antora-playbook_$lang.yml --attribute env=local
